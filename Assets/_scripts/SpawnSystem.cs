ee1d13973dc609c4f701bf3b50f8002e8b3d7132
Pforpeetar <phamanpeter@gmail.com>
2016-01-30 12:31:40 -0800

    Added Bull Prefab, Sprite, Edited Test Scene, Changed Main Camera Prefab, and Initial Spawning System
    Of Note:
    Spawning System will randomly spawn a bull on each of the lanes.

---
+++ Assets/_scripts/SpawnSystem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSystem : GGJBehaviour {
+    public int MaxDifficulty;
+
+    private float timer;
+    const int DefaultTimer = 2;
+    private float SpawnTimer;
+    private GameObject currentObstacle;
+    public GameObject[] obstacleList;
+    public int[] zRange = {0, 3, 6};
+
+    void Start()
+    {
+        SpawnTimer = DefaultTimer;
+    }
+
+    void Update()
+    {
+        Debug.Log("Timer: " + timer);
+        timer += Time.deltaTime;
+        if (timer >= SpawnTimer)
+        {
+            SpawnObstacle();
+            timer = 0;
+        }
+    }
+
+    void StartLevel()
+    {
+        
+    }
+
+    public void SpawnObstacle()
+    {
+        currentObstacle = obstacleList[Random.Range(0, obstacleList.Length)];
+	/* Determines Difficult of the interval of spawned objects
+	    while (currentObstacle.GetComponent<LaneableObject>() != null)
+        {
+            currentObstacle = obstacleList[Random.Range(0, obstacleList.Length)];
+        }
+    */
+        GameObject spawnedObstacle = (GameObject) GameObject.Instantiate(currentObstacle, new Vector3(Random.Range(-3, 3) - 10, 0, zRange[Random.Range(0, 3)]), Quaternion.identity);
+        //spawnedObstacle.GetComponent<LaneableObject>().setLane(Random.Range(0, 2));
+    }
+
+}
