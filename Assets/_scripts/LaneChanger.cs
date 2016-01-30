9beff15df90eb1ccbf353ff9e99825d90bf7b83c
German Garces <gegagome@gmail.com>
2016-01-30 10:46:06 -0800

    Initial player movement inside LaneChanger.cs

---
+++ Assets/_scripts/LaneChanger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneChanger : MonoBehaviour {
+
+	const int MOVE_UP = 2;
+	const int MOVE_DOWN = -2;
+
+	LaneOrder myLane;
+
+	void Start () {
+		// we could randomize lanes?
+		// talabamusic@me.com
+		myLane = LaneOrder.Lane1;
+	}
+
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.W)) {
+			MoveUp(MOVE_UP);
+		}
+		if (Input.GetKeyUp(KeyCode.S)) {
+			MoveDown(MOVE_DOWN);
+		}
+	}
+
+	void MoveUp (int zPosition) {
+		if (myLane == LaneOrder.Lane1) {
+			Move(zPosition);
+			myLane = LaneOrder.Lane2;
+		} else if  (myLane == LaneOrder.Lane2) {
+			Move(zPosition);
+			myLane = LaneOrder.Lane3;
+		}
+	}
+
+	void MoveDown (int zPosition) {
+		if  (myLane == LaneOrder.Lane3) {
+			Move(zPosition);
+			myLane = LaneOrder.Lane2;
+		} else if  (myLane == LaneOrder.Lane2) {
+			Move(zPosition);
+			myLane = LaneOrder.Lane1;
+		}
+	}
+
+	void Move (int moveZ) {
+		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveZ);
+	}
+}
---
+++ Assets/_scripts/Lanes.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum LaneOrder {
+	Lane1 = 1,
+	Lane2,
+	Lane3
+};
