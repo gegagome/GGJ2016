c7dca4b4b74b80cbabd57177d214cc1cd1f00b4f
Omari Straker <straka3000@gmail.com>
2016-01-30 13:59:34 -0800

    main menu stuff

---
+++ Assets/_scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (GUI.Button (new Rect (Screen.width / 3, 50, Screen.width / 4, Screen.height / 10), "StartGame")) {
+			Utilities.LoadLevel ("StageSelect");
+		} 
+		else if (GUI.Button (new Rect (Screen.width / 3, 150, Screen.width / 4, Screen.height / 10), "Credits")) {
+			Utilities.LoadLevel ("Credits");
+		} 
+	}
+}
