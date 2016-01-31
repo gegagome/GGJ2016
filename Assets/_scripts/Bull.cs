d14d6c3f441f50cc5a1e955c5271d31a721448bc
German Garces <gegagome@gmail.com>
2016-01-30 19:48:35 -0800

    Added rotation to the bulls' wheels

---
+++ Assets/_scripts/Bull.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bull : MonoBehaviour {
+
+	public GameObject frontWheel;
+	public GameObject rearWheel;
+	float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float rot;
+		rot =+ 10 * speed * Time.deltaTime;
+
+		frontWheel.transform.Rotate(new Vector3(0,rot,0));
+		rearWheel.transform.Rotate(new Vector3(0,rot,0));
+	}
+}
