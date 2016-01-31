af183795ddd8bc6ea166f7c2818734136e9c6ca7
Pforpeetar <phamanpeter@gmail.com>
2016-01-31 13:47:36 -0800

    Built SoundManager and added to running scene.

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -6,78 +6,101 @@ public class SoundManager : MonoBehaviour {
     public AudioClip[] bgList;
     public AudioSource effects;
     public AudioSource bg;
+
     public void playEffect(AudioClipSymbol symbol)
     {
         switch (symbol)
         {
             case AudioClipSymbol.AnnCD1:
                 effects.clip = effectsList[0];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnCD2:
                 effects.clip = effectsList[1];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnCD3:
                 effects.clip = effectsList[2];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnCD4:
                 effects.clip = effectsList[3];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnGoal:
                 effects.clip = effectsList[4];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnLv1:
                 effects.clip = effectsList[5];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnLv2:
                 effects.clip = effectsList[6];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnLv3:
                 effects.clip = effectsList[7];
+                effects.Play();
                 break;
             case AudioClipSymbol.AnnLv4:
                 effects.clip = effectsList[8];
                 break;
             case AudioClipSymbol.AnnRun:
                 effects.clip = effectsList[9];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerDeath1:
                 effects.clip = effectsList[10];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerDeath2:
                 effects.clip = effectsList[11];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerDeath3:
                 effects.clip = effectsList[12];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerDeath4:
                 effects.clip = effectsList[13];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerHit1:
                 effects.clip = effectsList[14];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerHit2:
                 effects.clip = effectsList[15];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerHit3:
                 effects.clip = effectsList[16];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump1:
                 effects.clip = effectsList[17];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump2:
                 effects.clip = effectsList[18];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump3:
                 effects.clip = effectsList[19];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump4:
                 effects.clip = effectsList[20];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump5:
                 effects.clip = effectsList[21];
+                effects.Play();
                 break;
             case AudioClipSymbol.PlayerJump6:
                 effects.clip = effectsList[22];
+                effects.Play();
                 break;
             default:
                 break;
@@ -89,16 +112,20 @@ public class SoundManager : MonoBehaviour {
         switch (symbol)
         {
             case BackgroundAudio.DeathStinger:
-                effects.clip = bgList[0];
+                bg.clip = bgList[0];
+                bg.Play();
                 break;
             case BackgroundAudio.Level1Loop:
-                effects.clip = bgList[1];
+                bg.clip = bgList[1];
+                bg.Play();
                 break;
             case BackgroundAudio.MainTitle:
-                effects.clip = bgList[2];
+                bg.clip = bgList[2];
+                bg.Play();
                 break;
             case BackgroundAudio.Victory:
-                effects.clip = bgList[3];
+                bg.clip = bgList[3];
+                bg.Play();
                 break;
             default:
                 break;
